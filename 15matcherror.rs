ef4faf479fbadd11d9b2d26d3bdcfa86a5192bf7
amogh <amoghbhattarai3205@gmail.com>
2026-02-05 18:32:41 +0545

    error handling with match in file

---
+++ 15matcherror.rs
@@ -0,0 +1,25 @@
+use std::io::BufRead;
+use std::io::BufReader;
+use std::fs::File;
+
+fn main(){
+    let file = File::open("non_existing_file.txt");
+    let file = match file{
+        Ok(file)=>file,
+        Err(error)=>{
+            match error.kind(){
+                std::io::ErrorKind::NotFound=>{
+                    panic!("File not found, error!")
+                }
+                _=>{
+                    panic!("Error opening file: {}",error)
+                }
+            }   
+        }
+    };
+
+    let reader = BufReader::new(file);
+    for  line in reader.lines(){
+        println!("{}", line.unwrap());
+    }
+}
