34fb309d7843f27ab5ac9ee9eef878364a3d9d6b
amogh <amoghbhattarai3205@gmail.com>
2026-02-18 23:52:35 +0545

    more on enums

---
+++ 25enumsTypse.rs
@@ -0,0 +1,36 @@
+#[derive(Debug)]
+
+enum Red{
+    Crimson,
+    Burgundy,
+    Maroon,
+}
+
+struct Color{
+    name: String,
+    shade: Red,
+}
+
+fn rndm_function(r: Red){
+    match r{
+        Red::Maroon => println!("Picked the correct shade!"),
+        _=> println!("Incorrect shade picked."),
+    }
+}
+
+fn main(){
+    let color1 = Color{
+        name: String::from("Red"),
+        shade: Red::Crimson,
+    };
+
+    let color2 = Color{
+        name: String::from("Red"),
+        shade: Red::Maroon,
+    };
+
+    println!("Color 1: {} of shade {:?}", color1.name, color1.shade);
+    println!("Color 2: {} of shade {:?}", color2.name, color2.shade);
+       
+    
+}
