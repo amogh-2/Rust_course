54dcb9dc7bc09ce35ae97c562b5a7800e3deb4b9
amogh <amoghbhattarai3205@gmail.com>
2026-02-20 17:20:32 +0545

    vector in enum

---
+++ 28enumsVector.rs
@@ -0,0 +1,19 @@
+enum Shape{
+    Circle(f64),
+    Square(f64),
+    Rectangle(f64,f64)
+}
+
+fn main(){
+    let shape = vec![Shape::Circle(5.0),Shape::Square(4.0),Shape::Rectangle(3.0,6.0)];
+    let total_area: f64 = shape
+        .iter()
+        .map(|shape| match shape{
+            Shape::Circle(radius) => std::f64::consts::PI*radius*radius,
+            Shape::Square(length) => length*length,
+            Shape::Rectangle(length,breadth) => length*breadth, 
+        })
+        .sum();
+
+    println!("The total area is: {:.2} sq. units ", total_area);
+}
